160346f8ea4d2d33d0ce49bdd53e3c9d24dc5d51
DannyB25 <dabrion25@gmail.com>
2024-07-09 14:48:35 -0700

    Pushing to GitHub

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,47 @@
+window.alert("Hello, world!"); //assigning value to command
+
+document.write('Hello, world!'); //assigning value to command
+
+var A = "This is a test!"; //assigning value to variable
+
+document.write(A); //assigning a variable/value to a method
+
+var B = "You call this a challenge!?"; //assigning a value to a variable
+
+alert(B); //assigning a value/variable to a method
+
+document.write("From Pete Townsend\'s pen, \"I can see for miles and miles and miles\""); //assigning a value to a method
+
+document.write("\"To be, " + "or not to be, " + "that is the question.\"" + " -Shakespeare"); //assigning a value to a method
+
+var C = "I" + " am" + " concatenating" + " a" + " string"; //assigning a value to a variable
+
+document.write(C); //assigning a value/variable to a method
+
+var Allman = "Band", Gregg = "Organ/Vocalist", Duane = "Guitar", Dickey = "Rhythym Guitar", Butch = "Drums", Jaimoe = "Drums"; //assigning values to variables
+document.write(Gregg); //assigning a variable/value to a method
+document.write(Butch); //assigning a variable/value to a method
+document.write(Dickey); //assigning a variable/value to a method
+document.write(20+20); //example of an expression in a statement 
+document.write(10-10); //example of an expression in a statement
+
+var Sent1 = "This is the beginning of the string"; //assigning value to variable
+var Sent2 = "and this is the end of the string"; //assigning value to variable
+var D = (Sent1) + (Sent2); //assigning value to variable
+document.write(D); //assigning value/variable to method
+document.write(2+2); //example of a statement and an expression
+
+function My_First_Function() {      //Defining the function and giving it a name
+    var str = "This is the button text!"; //giving the variable str a string value
+    document.getElementById("Button_Text").innerHTML = str; //putting the value of the 
+                                                            //variable into the HTML elementFromPoint
+                                                            //with the "Button_Text" id
+}
+
+
+
+
+
+
+
+
