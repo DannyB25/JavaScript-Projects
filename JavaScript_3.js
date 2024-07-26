e7bdcf27442b8264320b406b1e7c280bb18a7bae
DannyB25 <dabrion25@gmail.com>
2024-07-25 22:34:25 -0700

    push to github

--- JavaScript_3.js
+++ JavaScript_3.js
@@ -2,3 +2,26 @@ function displayType(guitars) {
     var guitarType= guitars.getAttribute("data-guitar-type");
     alert(guitars.innerHTML + " is made by the " + guitarType + " Guitar Company!");
 }
+
+function validateForm() {
+    let x = document.forms["myForm"]["name"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    let y = document.forms["myForm"]["number"].value;
+    if (y == "") {
+        alert("Number must be filled out");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
